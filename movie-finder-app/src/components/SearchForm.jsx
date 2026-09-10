f31b9b1da4822d0ecb23b40acf7cff8fdcffb1e3
nikitos <1>
2026-09-10 14:03:58 +0300

    minor changes

--- movie-finder-app/src/components/SearchForm.jsx
+++ movie-finder-app/src/components/SearchForm.jsx
@@ -1,9 +1,16 @@
+const API_KEY = import.meta.env.VITE_API_KEY
 
 function SearchForm() {
 
     return (
         <>
-        
+            <h2>Find a movie!</h2>
+
+            <input 
+                type="text" 
+            />
+
+            <button>Search</button>
         </>
     )
 }
